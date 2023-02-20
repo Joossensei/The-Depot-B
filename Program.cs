a5afceccffef6b6b1aac8c4ac8f50f7181d83f03
JBH-JORRIT <106533515+JORRIT-JBH@users.noreply.github.com>
2023-02-20 19:08:49 +0100

    Tadaaaaa
    Zo staat alles erin

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Test {
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var input2 = Console.ReadLine();
+
+            int int1 = 0;
+            int int2 = 0;
+
+            if (input != null && input2 != null){
+                int1 = Int32.Parse(input);
+                int2 = Int32.Parse(input2);
+            }
+
+            for (int i = int1; i < int2; i++)
+            {
+                string output = "";
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if(i % 5 != 0 && i % 3 != 0)
+                {
+                    output = i.ToString();
+                }
+
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
