0fd94c0a74f7a42330f2e59a86bae2aa49af5325
Jones <40595903+Joossensei@users.noreply.github.com>
2023-03-13 11:15:51 +0100

    Begin maken aan starten van een tour

---
+++ Classes/startTour.cs
@@ -0,0 +1,13 @@
+namespace startTour
+
+public class startTour {
+    public void selectTour() {
+
+    }
+    
+    // Vragen of iedereen zn barcode scant
+
+    // Checken of iedereen er is
+
+    // Tour updaten naar started
+}
