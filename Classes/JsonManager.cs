2d6d7fbe8135ccf901e91e8028b4c741c1abc1d9
JBH-JORRIT <106533515+JORRIT-JBH@users.noreply.github.com>
2023-03-20 11:29:44 +0100

    Fix that it searches for the alternative path

--- Classes/JsonManager.cs
+++ Classes/JsonManager.cs
@@ -22,8 +22,28 @@ namespace ReservationSystem;
                 }
 
             }
-            catch (FileNotFoundException)
+            catch (DirectoryNotFoundException)
             {
+                try 
+                {
+                    using(StreamReader reader = new StreamReader(@"..\..\..\JsonFiles\entryTickets.json"))
+                    {
+                        string json = reader.ReadToEnd();
+                        List<string> entryTickets = JsonConvert.DeserializeObject<List<string>>(json);
+                        foreach (string Entryt in entryTickets)
+                        {
+                            System.Console.WriteLine(Entryt);
+                        }
+                        Console.WriteLine(entryTickets);
+                        return entryTickets ?? new List<string>();
+                    }
+
+                }
+                catch (FileNotFoundException)
+                {
+                    System.Console.WriteLine("file 'entryTickets.json' not found.\nPut the file in JsonFiles directory");
+                    return new List<string>();
+                }
                 System.Console.WriteLine("file 'entryTickets.json' not found.\nPut the file in JsonFiles directory");
                 return new List<string>();
             }
