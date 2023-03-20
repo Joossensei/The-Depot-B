81e336dd917e0e4c05534b27519c6c7135712461
chris3007 <christian3007@outlook.com>
2023-03-20 15:45:03 +0100

    Write user to tours.json

--- Classes/makeReservation.cs
+++ Classes/makeReservation.cs
@@ -2,9 +2,40 @@ namespace ReservationSystem;
 
 class makeReservation
 {
+
+    public static void invalidReservation(string reason, Tour tour)
+    {
+
+        List<Action> actions = new List<Action>
+        {
+
+        };
+
+        Console.WriteLine(reason);
+        actions = new List<Action> {
+            new() {
+            text = "Nog eens proberen",
+            hasExtraBreak = false,
+            onAction = line => {
+                makeReservation.ReserveTour(Console.ReadLine(), tour);
+            }
+            },
+            new() {
+            text = "Teruggaan naar hoofdmenu",
+            hasExtraBreak = false,
+            onAction = line => {
+                //ProgramManger.setActions(getStartScreen());
+            }
+            }
+        };
+
+        ProgramManger.start(actions);
+    }
+
     public static void ReserveTour(string ticketID, Tour tour)
     {
-        List<Action> actions = new List<Action> {
+        List<Action> actions = new List<Action>
+        {
 
         };
 
@@ -21,37 +52,45 @@ class makeReservation
 
         if (validTicket == false)
         {
-
-            //Als de code niet in de beschikbare codes zit, laat de gebruiker het opnieuw proberen
-
-            Console.WriteLine("Deze code is niet valide. Probeer het opnieuw");
-            actions = new List<Action> {
-            new() {
-            text = "Nog eens proberen",
-            hasExtraBreak = false,
-            onAction = line => {
-                makeReservation.ReserveTour(Console.ReadLine(), tour);
-            }
-            },
-            new() {
-            text = "Teruggaan naar hoofdmenu",
-            hasExtraBreak = false,
-            onAction = line => {
-                //ProgramManger.setActions(getStartScreen());
-            }
-            }
-        };
+            invalidReservation("Deze code is ongeldig. Probeer het opnieuw", tour);
         }
         else
         {
 
             //Als de code wel goed is, check dan of er nog plek is in deze tour
-            Console.WriteLine(tour.id);
+
+            List<Tour> tours = new List<Tour>();
+
             foreach (var checkTour in jsonManager.LoadTours())
             {
-                if (checkTour.id == tour.id) {
-                    Console.WriteLine(tour.id);
+                if (checkTour.id == tour.id)
+                {
+                    Console.WriteLine("Test");
+
+                    if (checkTour.bookings.Count >= 13)
+                    {
+                        invalidReservation("Deze tour zit helaas al vol", tour);
+                    }
+                    else
+                    {
+                        checkTour.bookings.Add(new Booking
+                        {
+                            userId = ticketID,
+                            tourId = tour.id,
+                            createData = DateTime.Now,
+                            occupationStatus = OccupationStatus.Joined
+                        });
+                    }
                 }
+                else
+                {
+
+                }
+
+
+                tours.Add(checkTour);
+                var manager = new ReservationSystem.jsonManager();
+                manager.writeToJson(tours, @"JsonFiles/tours.json");
             }
         }
 
