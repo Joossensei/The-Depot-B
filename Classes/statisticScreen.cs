536414b4c53849b1a31eb85ce6b3e34963abbd03
chris3007 <christian3007@outlook.com>
2023-03-25 14:51:21 +0100

    Add initial class

---
+++ Classes/statisticScreen.cs
@@ -0,0 +1,44 @@
+namespace ReservationSystem;
+
+public class statisticScreen
+{
+    public static List<Action> getStatistics()
+    {
+        List<Action> actions = new(){
+                new (){
+                    text = "Voer een actie uit door het nummer voor de actie in te voeren.",
+                    hasExtraBreak = true
+                },
+                new (){
+                    text = $"Rondleidingen ({DateTime.Now.ToShortDateString()})",
+                    hasExtraBreak = true
+                }
+            };
+
+
+
+        //Add other statistics
+        actions.AddRange(new List<Action>(){
+                new (){ text = "\nBezoekers: 3242",},
+                new (){ text = "Rondleiding boekingen: 120",},
+                new (){ text = "Rondleiding aanwezigen: 112",},
+                new (){ text = "Rondleiding afwezigen: 8",},
+                new (){
+                    text = "Annuleringen: 34",
+                    hasExtraBreak = true,
+                },
+                new (){
+                    text = "Statistieken periode",
+                    onAction = line => {}
+                },
+                new (){
+                    text = "Terug naar start",
+                    onAction = line => {
+                        ProgramManger.setActions(Program.getStartScreen());
+                    }
+                },
+            });
+
+        return actions;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -175,43 +175,7 @@ namespace ReservationSystem
 
         static List<Action> getStatistics()
         {
-            List<Action> actions = new(){
-                new (){
-                    text = "Voer een actie uit door het nummer voor de actie in te voeren.",
-                    hasExtraBreak = true
-                },
-                new (){
-                    text = $"Rondleidingen ({DateTime.Now.ToShortDateString()})",
-                    hasExtraBreak = true
-                }
-            };
-
-            //Add tours from today
-            actions.AddRange(getTours(hasActions: false));
-
-            //Add other statistics
-            actions.AddRange(new List<Action>(){
-                new (){ text = "\nBezoekers: 3242",},
-                new (){ text = "Rondleiding boekingen: 120",},
-                new (){ text = "Rondleiding aanwezigen: 112",},
-                new (){ text = "Rondleiding afwezigen: 8",},
-                new (){
-                    text = "Annuleringen: 34",
-                    hasExtraBreak = true,
-                },
-                new (){
-                    text = "Statistieken periode",
-                    onAction = line => {}
-                },
-                new (){
-                    text = "Terug naar start",
-                    onAction = line => {
-                        ProgramManger.setActions(getStartScreen());
-                    }
-                },
-            });
-
-            return actions;
+            return statisticScreen.getStatistics();
         }
 
 
