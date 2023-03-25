74465aef4bd98cf54ee9333447877bc5066cacb4
chris3007 <christian3007@outlook.com>
2023-03-25 15:30:44 +0100

    Get data from tours.json

--- Classes/statisticScreen.cs
+++ Classes/statisticScreen.cs
@@ -4,9 +4,32 @@ public class statisticScreen
 {
     public static List<Action> getStatistics()
     {
-
-
-        
+        List<Tour> tours = Program.tours;
+        int totalVisitors = jsonManager.LoadEntryTickets().Count();
+        int totalReservations = 0;
+        int cancelledReservations = 0;
+        int joinedReservations = 0;
+        int visitedReservations = 0;
+        foreach (Tour tour in tours)
+        {
+            foreach (Booking reservation in tour.bookings)
+            {
+                totalReservations++;
+                switch (reservation.occupationStatus)
+                {
+                    case OccupationStatus.Joined:
+                        joinedReservations++;
+                        break;
+                    case OccupationStatus.Canceled:
+                        cancelledReservations++;
+                        Console.WriteLine(reservation.userId);
+                        break;
+                    case OccupationStatus.Visited:
+                        joinedReservations++;
+                        break;
+                }
+            }
+        }
 
         List<Action> actions = new(){
                 new (){
@@ -23,12 +46,12 @@ public class statisticScreen
 
         //Add other statistics
         actions.AddRange(new List<Action>(){
-                new (){ text = "\nBezoekers: 3242",},
-                new (){ text = "Rondleiding boekingen: 120",},
-                new (){ text = "Rondleiding aanwezigen: 112",},
-                new (){ text = "Rondleiding afwezigen: 8",},
+                new (){ text = $"\nBezoekers: {totalVisitors}",},
+                new (){ text = $"Rondleiding boekingen: {totalReservations}",},
+                new (){ text = $"Rondleiding aanwezigen: {visitedReservations}",},
+                new (){ text = $"Rondleiding afwezigen: {joinedReservations}",},
                 new (){
-                    text = "Annuleringen: 34",
+                    text = $"Annuleringen: {cancelledReservations}",
                     hasExtraBreak = true,
                 },
                 new (){
