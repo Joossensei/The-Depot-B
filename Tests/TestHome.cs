4ce6bdd8a5a6b11e1f2500c200d233b6d5aa03f5
JBH-JORRIT <106533515+JORRIT-JBH@users.noreply.github.com>
2023-04-20 21:56:46 +0200

    Creating test for test

---
+++ Tests/TestHome.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using project_depotv1.Classes;
+using ReservationSystem;
+
+namespace project_depotv1.Tests;
+
+[TestFixture]
+public class ProgramManagerTests
+{
+    private ConsoleManagerStub _consoleManager = null;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _consoleManager = new ConsoleManagerStub();
+        ProgramManger.start(Program.getStartScreen());
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        
+    }
+    
+    [TestCase("")]
+    public void RunWithInputAs1AndName(string name)
+    {
+        _consoleManager.UserInputs.Enqueue("1");
+        Assert.AreEqual("1", "1");
+    }
+}
